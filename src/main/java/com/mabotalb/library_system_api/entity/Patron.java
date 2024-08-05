9783d772075c0088d1c3e832a948b6a51d6b3b26
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-08-05 18:51:31 +0300

    Add PatronEntity file

---
+++ src/main/java/com/mabotalb/library_system_api/entity/Patron.java
@@ -0,0 +1,26 @@
+package com.mabotalb.library_system_api.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "patrons")
+public class Patron {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 100)
+    @Pattern(regexp = "^[a-zA-Z\\s]+$", message = "Patron name must contain only letters and spaces")
+    @Column(name = "name")
+    private String name;
+
+    @NotBlank(message = "Contact information is required")
+    @Email(message = "Contact information must be a valid email address")
+    @Column(name = "contact_information", unique = true)
+    private String contactInformation;
+}
