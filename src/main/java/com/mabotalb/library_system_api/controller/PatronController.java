f4d5a680d201dcb5dbeacab7afc07f337da67c4a
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-08-05 18:58:03 +0300

    Add PatronController file

--- src/main/java/com/mabotalb/library_system_api/controller/PatronController.java
+++ src/main/java/com/mabotalb/library_system_api/controller/PatronController.java
@@ -21,28 +21,28 @@ public class PatronController {
     private PatronService patronService;
 
     @GetMapping
-    public ResponseEntity<ApiResponse<List<Patron>>> getAllBooks() {
+    public ResponseEntity<ApiResponse<List<Patron>>> getAllPatrons() {
         List<Patron> patrons = patronService.getAllPatrons();
         ApiResponse<List<Patron>> response = new ApiResponse<>("Patrons fetched successfully!", patrons);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse<Patron>> getBookById(@PathVariable @Positive Long id) {
+    public ResponseEntity<ApiResponse<Patron>> getPatronById(@PathVariable @Positive Long id) {
         Patron patron = patronService.getPatron(id);
         ApiResponse<Patron> response = new ApiResponse<>("Patron fetched successfully!", patron);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping
-    public ResponseEntity<ApiResponse<Patron>> AddBook(@Valid @RequestBody Patron patronDetails) {
+    public ResponseEntity<ApiResponse<Patron>> AddPatron(@Valid @RequestBody Patron patronDetails) {
         Patron savedPatron = patronService.addPatron(patronDetails);
         ApiResponse<Patron> response = new ApiResponse<>("Patron added successfully!", savedPatron);
         return ResponseEntity.status(201).body(response);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ApiResponse<Patron>> updateBookById(
+    public ResponseEntity<ApiResponse<Patron>> updatePatronById(
             @PathVariable @Positive Long id,
             @Valid @RequestBody Patron patronDetails) {
         Patron updatedPatron = patronService.updatePatron(id, patronDetails);
@@ -51,7 +51,7 @@ public class PatronController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteBookById(@PathVariable @Positive Long id) {
+    public ResponseEntity<String> deletePatronById(@PathVariable @Positive Long id) {
         patronService.deletePatron(id);
         return ResponseEntity.ok("Patron deleted successfully!");
     }
